8793606063ca56be7080c6460339dd3d489e235a
Christine Miaekivi <christine.miaekivi@gmail.com>
2025-08-08 11:58:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="app">
+  Hello
+</div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+}
+</style>
