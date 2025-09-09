c03711422830e03dc4d31d56f3e9a8982a2884a8
Christine Miaekivi <christine.miaekivi@gmail.com>
2025-09-09 13:35:53 +0200

    added forecast store

---
+++ src/stores/weatherForecast.ts
@@ -0,0 +1,37 @@
+import { computed, ref } from "vue"
+import { defineStore } from "pinia"
+import type { ForecastDto } from "@/types"
+import type { ShortForecastDto } from "@/types"
+import { getDetailedForecast, getShortForecast } from "@/services/weather-service"
+
+export const useWeatherForecastStore = defineStore("weatherForecast", () => {
+  const detailedForecastDto = ref<ForecastDto | null>(null)
+  const shortForecastDto = ref<ShortForecastDto | null>(null)
+  const loadingDetailed = ref(false)
+  const errorDetailed = ref<string | null>(null)
+
+  async function fetchShortForecast(){
+    shortForecastDto.value = await getShortForecast()
+  }
+
+  async function fetchDetailedForecast(city: string) {
+    loadingDetailed.value = true
+    errorDetailed.value = null
+    try {
+      detailedForecastDto.value = await getDetailedForecast(city)
+    } catch (err) {
+      errorDetailed.value = "Failed to detailed weather forecast"
+    } finally {
+      loadingDetailed.value = false
+    }
+  }
+
+  return {
+    detailedForecastDto,
+    shortForecastDto,
+    loadingDetailed,
+    errorDetailed,
+    fetchDetailedForecast,
+    fetchShortForecast
+  }
+})
