a0565d92e565f9c4a98d3d3e1071fa907c84fa80
Christine Miaekivi <christine.miaekivi@gmail.com>
2025-09-09 13:36:25 +0200

    integrated weather api

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -6,12 +6,36 @@ const apiClient = axios.create({
 
 export async function getCurrentWeather(city: string) {
   try {
-  const response = await apiClient.get(`/weather`, {
-    params: { city },
-  });
+    const response = await apiClient.get(`/weather`, {
+      params: { city },
+    })
     return response.data;
   } catch (error) {
     console.error("Error fetching weather:", error);
-    throw error;
+    throw error
   }
 }
+
+export async function getDetailedForecast(city: string) {
+  try {
+    const response = await apiClient.get(`/weather/detailedForecast`, {
+      params: { city },
+    })
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather:", error);
+    throw error
+  }
+}
+
+export async function getShortForecast() {
+  try {
+    const response = await apiClient.get(`/weather/shortForecast`)
+    return response.data
+  } catch (error) {
+    console.error("Error fetching weather:", error);
+    throw error
+  }
+}
+
+
